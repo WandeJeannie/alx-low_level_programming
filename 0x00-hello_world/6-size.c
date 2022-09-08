117853b73f0eb02ed2f74efcd6620aeac9c33097
WandeJeannie <wandeja6293@gmail.com>
2022-09-08 15:04:00 -0700

    hello world for C programing language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+include<stdio.h>
+
+int main() {
+
+
+		        char charType;
+			int intType;
+			long int longInt;
+			long long int longIntType;
+			float floatType;
+				
+
+                                                                        
+                       
+		             	   
+			    
+			    
+			    
+			    
+			                     printf("Size of char: %lu byte\n", sizeof(charType));
+					     printf("size of an int: %lu byte\n",sizeof(intType));
+					     printf("size of a long int: %lu byte\n",sizeof(longInt));
+					     printf("size of a long long int: %lu byte\n",sizeof(longIntType));
+					     printf("size of a float: %lu byte\n",sizeof(floatType));
+			    
+			                         
+			   
+			                            return 0;
+             }
