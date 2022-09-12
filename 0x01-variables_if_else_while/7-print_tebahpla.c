1752ded4ea6ae49e4e137acf2a16bcbc2ffd4289
WandeJeannie <wandeja6293@gmail.com>
2022-09-12 07:13:50 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,16 +3,16 @@
 
 /**
  * main - Entry point
- * Description: prints all single digit numbers of base 10 starting from 0
- * Return 0
+ * Description: prints the lowercase alphabet in reverse
+ * Return: 0
  */
 int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = 'z'; a >= 'a'; a--)
 	{
-		putchar(a + '0');
+		putchar(a);
 	}
 	putchar('\n');
 	return (0);
