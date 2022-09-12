678b4f508e04b7466a4df6a1b4118a8ecc9c6d56
WandeJeannie <wandeja6293@gmail.com>
2022-09-12 07:46:50 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of single-digit numbers
+ * Return 0
+ */
+
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
