6b59054463f9e16fa77f4855b15fc41484a3f2f9
WandeJeannie <wandeja6293@gmail.com>
2022-09-14 01:00:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints the alphabet, in lowercase\n
+ * Return: is 0.
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
